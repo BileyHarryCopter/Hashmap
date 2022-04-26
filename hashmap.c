57b52bc6bda2f36178f8fb11b13d060478d863d9
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-26 14:19:35 +0300

    working hashmap without any memleaks

--- hashmap.c
+++ hashmap.c
@@ -2,49 +2,42 @@
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
               //   Announcement the structer of hashmap    //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-typedef struct node_t
+struct list_t
 {
-    struct node_t *next;
+    struct list_t *next;
     data_t         data;
-} node_t;
+};
 
 struct hashmap
 {
-    node_t         **array;
+    list_t        **array;
     unsigned      capacity;
     unsigned      insertion;
-    unsigned      (*hash_calc) (data_t data);
+    unsigned      (*hashcalc) (data_t data);
 };
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-hashmap *Hash_Ctor (const unsigned size_init, unsigned (*Hash_Calc)(data_t data))
+hashmap *HashCtor (const unsigned size_init, unsigned (*hashcalc)(data_t data))
 {
     int i = 0;
-    hashmap *hshmp = (hashmap *)calloc (1, sizeof (hashmap));
+    hashmap *hshmp = (hashmap *) calloc (1, sizeof (hashmap));
 
     hshmp->capacity    = size_init;
     hshmp->insertion   = 0;
-    hshmp->array       = (node_t **) calloc (size_init, sizeof (node_t *));
-    for (i = 0; i < size_init; i++)
-    {
-        hshmp->array[i] = (node_t *) calloc (1, sizeof (node_t));
-        #if 0
-        hshmp->array[i]->data = (data_t) calloc (STRLEN, sizeof (char));
-        #endif
-    }
-    hshmp->hash_calc = Hash_Calc;
+    hshmp->array       = (list_t **) calloc (size_init, sizeof (list_t *));
+    hshmp->hashcalc = HashCalc;
 
     return hshmp;
 }
 
-int Hash_Fill (int mode, ...)
+int HashFill (int mode, ...)
 {
-    va_list args;
+    va_list        args;
 
-    char symb;
-    char *str;
-    char *fname;
-    hashmap *hshmp;
+    char           symb;
+    char           *str;
+    char         *fname;
+    hashmap      *hshmp;
     unsigned  find_numb;
     unsigned  size_buff;
     unsigned  stri_numb;
@@ -54,7 +47,7 @@ int Hash_Fill (int mode, ...)
     hshmp = va_arg (args, hashmap*);
     assert (hshmp);
 
-    if (mode == FSTDIO)
+    if (mode == CONTEST)
     {
         scanf ("%u %u", &find_numb, &size_buff);
         while (getchar () != '\n') {;}
@@ -64,8 +57,10 @@ int Hash_Fill (int mode, ...)
             if (isalpha (symb))
             {
                 str = strctor (symb);
+                // printf ("From %s we have %s with pointer: %p\n", __PRETTY_FUNCTION__, str, str);
                 position += strlen (str);
-                Hash_Insrt (hshmp, str);
+                HashInsrt (hshmp, str);
+                strdel (str);
             }
         }
     }
@@ -76,54 +71,56 @@ int Hash_Fill (int mode, ...)
         if (!file)
             return INPUT_ERROR;
         fseek (file, 0L, SEEK_SET);
-        assert (fscanf (file ,"%u %u", &find_numb, &size_buff) == 2);
+
+
         while (fgetc (file) != '\n') {;}
-        for (position = 0; position < size_buff; position++)
+        for (symb = 1; feof (file) && symb != EOF;)
         {
             symb = fgetc (file);
-            if (isalpha (symb) != 0)
+            if (!isalpha (symb))
             {
                 str = fstrctor (symb, file);
                 position += strlen (str);
-                Hash_Insrt (hshmp, str);
+                HashInsrt (hshmp, str);
+                strdel (str);
             }
         }
         fclose (file);
     }
     else
+    {
+        va_end (args);
         return INPUT_ERROR;
+    }
 
     va_end (args);
     return NO_ERROR;
 }
 
-unsigned Hash_Find (hashmap hshmp, data_t data)
+unsigned HashFind (const hashmap *hshmp, data_t data)
 {
-    assert (&hshmp);
-    unsigned key = (*hshmp.hash_calc)(data) % (hshmp.capacity);
+    assert (hshmp);
+    unsigned key = (hshmp->hashcalc)(data) % (hshmp->capacity);
     unsigned conv, pos = 0;
-    node_t *list = hshmp.array[key];
+    list_t *list = hshmp->array[key];
 
-    if (!(list->next))
+    if (!list)
         return 0;
     else
     {
         conv = 0;
-        list = list->next;
-        while (list->next != NULL)
+        while (list)
         {
-            if (!istrcmp (list->data, data))
+            if (istrcmp (list->data, data) == 0)
                 conv++;
             list = list->next;
         }
-        if (!istrcmp (list->data, data))
-            conv++;
     }
 
     return conv;
 }
 
-int Contest_Task (hashmap *hshmp)
+int ContestTask (hashmap *hshmp)
 {
     char symb;
     char *str;
@@ -139,113 +136,141 @@ int Contest_Task (hashmap *hshmp)
         if (isalpha (symb))
         {
             str = strctor (symb);
-            printf ("%u ", Hash_Find (*hshmp, str));
+            printf ("%u ", HashFind (hshmp, str));
             position += strlen (str);
+            strdel (str);
         }
     }
     printf ("\n");
     return NO_ERROR;
 }
 
-unsigned Hash_Calc (data_t data)
+unsigned HashCalc (data_t data)
 {
-    unsigned hash_key = 5381;
+    unsigned hashkey = 5381;
     while (*data != '\0')
     {
-        hash_key = ((hash_key << 5) + hash_key) + *data;
+        hashkey = ((hashkey << 5) + hashkey) + *data;
         data++;
     }
-    return hash_key;
+    return hashkey;
 }
 
-int Hash_Insrt (hashmap *hshmp, data_t insrt_data)
+int HashInsrt (hashmap *hshmp, data_t insrt_data)
 {
-    unsigned hash_key = 0;
-    node_t   *list    = (node_t *) calloc (1, sizeof (node_t));
+    //  initialization of new node of list  //
+    list_t   *list    = (list_t *) calloc (1, sizeof (list_t));
+    list->data        = (data_t) calloc (strlen (insrt_data) + 1, sizeof (char));
+    list->data        = memmove (list->data, insrt_data, strlen (insrt_data));
+    // printf ("From %s we have %s: pointer data %p; pointer list %p\n", __PRETTY_FUNCTION__, list->data, list->data, list);
 
+    //  checking for resize  //
     if (hshmp->insertion > CRIT_KOEF * hshmp->capacity)
-        Hash_Resz (hshmp);
-    hash_key = (*hshmp->hash_calc)(insrt_data) % (hshmp->capacity);
+        HashResz (hshmp);
 
-    assert (hshmp->array[hash_key]);
+    assert (hshmp->array);
 
-    list->next = hshmp->array[hash_key]->next;
-    list->data = insrt_data;
-    hshmp->array[hash_key]->next = list;
+    unsigned hashkey = (*hshmp->hashcalc)(insrt_data) % (hshmp->capacity);
 
-    hshmp->insertion += 1;
-    return NO_ERROR;
-}
-
-int Hash_Resz (hashmap *hshmp)
-{
-    unsigned i = 0;
-    node_t *first, *second;
-    hashmap *new_hshmp = Hash_Ctor (ENCR_KOEF * hshmp->capacity, hshmp->hash_calc);
-
-    for (i = 0; i < hshmp->capacity; i++)
+    if (!hshmp->array[hashkey])
+        hshmp->array[hashkey] = list;
+    else
     {
-        first = hshmp->array[i];
-        if (first->next == NULL)
-        {
-            free (first);
-            continue;
-        }
-        first = first->next;
-        free (hshmp->array[i]);
-        while (first->next != NULL)
-        {
-            Hash_Insrt (new_hshmp, first->data);
-            second = first;
-            first = first->next;
-            free (second);
-        }
-        Hash_Insrt (new_hshmp, first->data);
-        free (first);
+        list->next = hshmp->array[hashkey];
+        hshmp->array[hashkey] = list;
     }
 
-    free (hshmp->array);
-    hshmp->array     = new_hshmp->array;
-    hshmp->capacity  = new_hshmp->capacity;
-    hshmp->insertion = new_hshmp->insertion;
-    hshmp = new_hshmp;
-
+    hshmp->insertion++;
     return NO_ERROR;
 }
 
-int Hash_Dtor (hashmap *hshmp)
+int HashResz (hashmap *hshmp)
 {
-    int i = 0;
-    node_t *first  = NULL;
-    node_t *second = NULL;
-
     assert (hshmp);
 
-    for (i = 0; i < hshmp->capacity; i++)
-    {
-        first = hshmp->array[i];
-        while (first->next != NULL)
-        {
-            second = first;
-            first  = first->next;
-            free (second->data);
-            free (second);
-        }
+    //  initialization of sliders
+    list_t *first, *second;
 
-        if (first->data != NULL)
-            free (first->data);
-        free (first);
+    //  initialization of new array
+    unsigned new_capacity = ENCR_KOEF*hshmp->capacity;
+    list_t **new_array = calloc (new_capacity, sizeof (list_t *));
+
+    for (unsigned ip = 0; ip < hshmp->capacity; ip++)
+    {
+        if (hshmp->array[ip] == NULL)
+            continue;
+        else
+        {
+            first = hshmp->array[ip];
+            while (first)
+            {
+                second = first;
+
+                //  initialization of a node for list in new array  //
+                list_t *nnode  = calloc (1, sizeof (list_t));
+                nnode->data   = calloc (strlen(second->data) + 1, sizeof (char));
+                nnode->data   = memmove (nnode->data, second->data, strlen(second->data));
+
+                //  insertion of a node in new array  //
+                unsigned nkey = (*hshmp->hashcalc) (nnode->data) % (new_capacity);
+                if (!new_array[nkey])
+                    new_array[nkey] = nnode;
+                else
+                {
+                    nnode->next = new_array[nkey];
+                    new_array[nkey] = nnode;
+                }
+
+                //  deleting data in old array  //
+                free (second->data);
+                free (second);
+
+                first = first->next;
+            }
+        }
     }
+
+
+    free (hshmp->array);
+    hshmp->array    = new_array;
+    hshmp->capacity = new_capacity;
+
+    return NO_ERROR;
+}
+
+int HashDtor (hashmap *hshmp)
+{
+    assert (hshmp);
+    list_t *first, *second;
+
+
+    for (unsigned ip = 0; ip < hshmp->capacity; ip++)
+    {
+        first = hshmp->array[ip];
+        if (!first)
+            continue;
+        else
+        {
+            while (first)
+            {
+                second = first;
+                free (second->data);
+                free (second);
+                first = first->next;
+            }
+        }
+    }
+
     free (hshmp->array);
     free (hshmp);
 
     return NO_ERROR;
 }
 
-void Hash_Dump (hashmap *hshmp)
+void HashDump (hashmap *hshmp)
 {
     int i = 0;
-    node_t *list;
+    list_t *list;
 
     printf ("_HASHMAP_\n");
     printf ("\tCAPACITY:   %u\n", hshmp->capacity);
@@ -291,6 +316,13 @@ char *strctor (char first)
     }
     *(str + length) = '\0';
     return str;
+}
+
+int strdel (char * str)
+{
+    assert (str);
+    free (str);
+    return NO_ERROR;
 }
 
 int istrcmp (const char * str1, const char * str2)
--- hashmap.h
+++ hashmap.h
@@ -14,36 +14,38 @@
 enum
 {
     NO_ERROR    = 0,
-    INPUT_ERROR = 1,
-    FSTDIO      = 2,
-    FFILE       = 3,
-    SIZE_INIT   = 10,
+    ERROR,
+    INPUT_ERROR,
+    CONTEST,
+    FFILE,
+    SIZE_INIT   = 100,
     STRLEN      = 50
 };
 
 typedef char* data_t;
-struct node_t;
+typedef struct list_t list_t;
 typedef struct hashmap hashmap;
 
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-hashmap *Hash_Ctor (const unsigned size_init, unsigned (*Hash_Calc)(data_t data));
-int Hash_Resz (hashmap *hshmp);
-int Hash_Dtor (hashmap *hshmp);
-unsigned Hash_Find (hashmap hshmp, data_t data);
+hashmap *HashCtor (const unsigned size_init, unsigned (*hashcalc)(data_t data));
+int HashResz (hashmap *hshmp);
+int HashDtor (hashmap *hshmp);
+unsigned HashFind (const hashmap *hshmp, data_t data);
 
-int Hash_Fill (int mode, ...);
+int HashFill (int mode, ...);
 
-int Hash_Insrt (hashmap *hshmp, data_t insrt_data);
+int HashInsrt (hashmap *hshmp, data_t insrt_data);
 
 
 // int Contest_Task (hashmap hshmp, const char* name);
-int Contest_Task (hashmap *hshmp);
+int ContestTask (hashmap *hshmp);
 
 
-void Hash_Dump (hashmap *hshmp);
-unsigned Hash_Calc (data_t data);
+void HashDump (hashmap *hshmp);
+unsigned HashCalc (data_t data);
 
+int strdel (char * str);
 char *strctor  (char first);
 char *fstrctor (char first, FILE * file);
 int istrcmp (const char * str1, const char * str2);
