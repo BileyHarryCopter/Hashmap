f1b8c6028bec195887203f7f9d416bf94e218f90
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-29 14:07:06 +0300

    make

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include "include/hashmap.h"
+#include "hashmap.h"
 
 int main(int argv, char *argc[]) {
   hashmap *hshmp;
   hshmp = HashCtor(SIZE_INIT, HashCalc);
   HashFill(CONTEST, hshmp);
-  // ContestTask (hshmp);
-
+  ContestTask(hshmp);
+  // Сontest work
   // HashDump (hshmp);
   HashDtor(hshmp);
   return 0;
